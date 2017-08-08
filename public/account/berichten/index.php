061cec72b986e4bff36698135b2ef47a5332c59d
Villermen <villermen@gmail.com>
2017-08-08 23:36:48 +0200

    Messages page

--- public/account/berichten/index.php
+++ public/account/berichten/index.php
@@ -1,48 +1,25 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-	<?php
-	error_reporting(false);
-	include("D:/website/metadata.txt");
-	?>
-	<title>Toxic - Leden panel - Berichten</title>
-</head>
-
-<body>
-
 <?php
-include("D:/website/menu.php");
-include("D:/website/accountmenu.php");
-include("D:/website/account/functies.php");
+    $titel = "Toxic - Leden panel - Berichten";
+    require("../../includes/pagestart.php");
+
+    $permission = $login && stripos($login["privileges"], "M") !== false;
 ?>
 
 <h1>Leden panel</h1>
 
-<?php
+<h2>Berichten <span style="color: red">(werkt nog niet)</span></h2>
 
-//indien error met database
-if (substr_count($bevoegdheid=bevoegdheid(),"<p>")>0)
-	echo $bevoegdheid;
-elseif (substr_count($bevoegdheid,"M")==0) //geen toegang
-	{
-	echo "<p>Je hebt niet de bevoegdheid om berichten te maken/bekijken.</p>
-		  <p>Klik <a href='/account'>hier</a> om terug te gaan naar het panel</p>";
-	}
-else
-	{
-	echo "
-	<h2>Berichten</h2>
-	
-	<p><a href='nieuw_bericht.php'>Nieuw Bericht</a><br />
-	<br />
-	<a href='inbox.php'>Inbox</a><br />
-	<br />
-	<a href='verzonden_berichten.php'>Verzonden berichten</a></p>";
-	}
-	
-?>
-	
-</body>
+<?php if ($permission): ?>
+    <p>
+        <a href="nieuw_bericht.php">Nieuw Bericht</a><br />
+        <br />
+        <a href="inbox.php">Inbox</a><br />
+        <br />
+        <a href="verzonden_berichten.php">Verzonden berichten</a>
+    </p>
+<?php else: ?>
+    <p>Je hebt niet de bevoegdheid om berichten te maken/bekijken.</p>
+    <p>Klik <a href="<?=$basePath;?>account">hier</a> om terug te gaan naar het panel</p>
+<?php endif; ?>
 
-</html>
+<?php require("../../includes/pageend.php"); ?>
