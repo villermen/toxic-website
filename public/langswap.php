730445891e3e0ffd975ce2a1d1dfd0e7d87b7f96
Villermen <villermen@gmail.com>
2017-08-09 01:56:08 +0200

    Minor adjustment to langswap cookie and redirect

--- public/langswap.php
+++ public/langswap.php
@@ -1,5 +1,8 @@
 <?php
 
+$title1 = "Toxic - Langswap";
+require("includes/pagestart.php");
+
 if (!isset($_COOKIE["lang"])) {
     $langswap = 2;
 } elseif ($_COOKIE["lang"] == 2) {
@@ -8,5 +11,14 @@ if (!isset($_COOKIE["lang"])) {
     $langswap = 2;
 }
 
-setcookie("lang", $langswap,time()+60*60*24*365);
-header("Location: ".$_SERVER["HTTP_REFERER"]);
+setcookie("lang", $langswap, time()+60*60*24*365, $basePath);
+
+if (isset($_SERVER["HTTP_REFERER"]) && $_SERVER["HTTP_REFERER"]) {
+    $redirect = $_SERVER["HTTP_REFERER"];
+} else {
+    $redirect = $basePath;
+}
+
+header("Location: " . $redirect);
+
+require("includes/pageend.php");
